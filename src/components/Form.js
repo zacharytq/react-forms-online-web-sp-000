d5a3c55f204dc5e126bd31051f6ff393117d8d4b
zacharytq <zacharytq@gmail.com>
2021-03-03 14:43:17 -0500

    commit tree 7c78c6eeb305c89f03ade277556d9b73cb8af6c6

--- src/components/Form.js
+++ src/components/Form.js
@@ -18,9 +18,15 @@ class Form extends React.Component {
     })
   }
 
+  handleSubmit = event => {
+    event.preventDefault()
+    let formData = { firstName: this.state.firstName, lastName: this.state.lastName }
+    this.sendFormDataSomewhere(formData)
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={event => this.handleSubmit(event)}>
         <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
         <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
       </form>
